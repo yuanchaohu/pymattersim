faf921b5942a08fe46f2537dd748fc67b407b451
yuanchaohu <ychu0213@gmail.com>
2023-11-08 22:00:13 +0800

    update util functions

--- utils/funcs.py
+++ utils/funcs.py
@@ -1,6 +1,17 @@
 # coding = utf-8
 
-"""small mathmatical functions for feasible computation"""
+"""mathmatical functions for feasible computation"""
+
+import numpy as np
+from utils.logging_utils import get_logger_handle
+
+logger = get_logger_handle(__name__)
+
+# pylint: disable=invalid-name
+
+def kronecker(i: int, j: int) -> int:
+    """Kronecker function"""
+    return int(i==j)
 
 def nidealfac(ndim: int=3) -> float:
     """
@@ -15,4 +26,33 @@ def nidealfac(ndim: int=3) -> float:
     if ndim == 3:
         return 4.0 / 3
     elif ndim == 2:
-        return 1.0 
+        return 1.0
+
+def moment_of_inertia(
+    positions: np.ndarray,
+    m: int=1,
+    matrix: bool=False
+) -> np.ndarray:
+    """
+    moment of inertia for a rigid body made of n points / particles
+
+    Inputs:
+        1. positions (np.ndarray): positions of the point particles as [numofatoms, 3]
+        2. m (int): assuming each point mass is 1.0/numofatoms
+        3. matrix (bool): return the results as a matrix of [ixx iyy izz ixy ixz iyz]
+    
+    Return:
+        moment of inertia (np.ndarray)
+    """
+
+    Iij = np.zeros((3, 3))
+    distance2 = np.square(positions).sum(axis=1)
+    for i in range(3):
+        for j in range(3):
+            Iij[i, j] = m*(distance2*kronecker(i, j)-positions[:, i]*positions[:, j]).sum()
+    
+    Iij /= positions.shape[0]
+    if matrix:
+        return Iij
+    else:
+        return np.ndarray([Iij[0,0], Iij[1,1], Iij[2,2], Iij[0,1], Iij[0,2], Iij[1,2]])
--- utils/geometry.py
+++ utils/geometry.py
@@ -1,29 +1,38 @@
 #coding = utf-8
 
 """
-develop tiny (math) functions to assist other analysis
+math geometrical functions to assist other analysis
 see documentation @ ../docs/utils.md
 """
 
 import numpy as np
 from utils.pbc import remove_pbc
+from utils.logging_utils import get_logger_handle
+
+logger = get_logger_handle(__name__)
 
 # pylint: disable=invalid-name
 
-def triangle_area(positions: np.array, hmatrix: np.array, ppp: list=[1,1]) -> np.array:
+def triangle_area(
+    positions: np.ndarray,
+    hmatrix: np.ndarray,
+    ppp: list=[1,1]
+) -> float:
     """
-    claculate the area of a triangle by using Heron's equation
+    claculate the area of a triangle using Heron's equation
 
     Inputs:
-        1. positions (np.array): numpy array of particle positions,
+        1. positions (np.ndarray): numpy array of particle positions,
                                  shape=(3, 2) for 2D, shape=(3, 3) for 3D
-        2. hmatrix (np.array): h-matrix of the box
+        2. hmatrix (np.ndarray): h-matrix of the box
         3. ppp (list): the periodic boundary conditions, setting 1 for yes and 0 for no
-                       default [1, 1], that is, PBC is applied in all three dimensions for 3D box
+                       default [1, 1], that is, PBC is applied in all two dimensions
 
     Return:
-        area of triangle (np.array)             
+        area of triangle (float)             
     """
+    logger.info(f"Calculate triangle area in a {len(ppp)}-dimensional system")
+
     point1 = positions[0]
     point2 = positions[1]
     point3 = positions[2]
@@ -39,7 +48,7 @@ def triangle_area(positions: np.array, hmatrix: np.array, ppp: list=[1,1]) -> np
     S = np.sqrt(p*(p-R12)*(p-R13)*(p-R23))
     return S
 
-def triangle_angle(a: float, b: float, c: float) -> np.array:
+def triangle_angle(a: float, b: float, c: float) -> float:
     """
     calculate the angle of a triangle based on side lengths
 
@@ -47,66 +56,79 @@ def triangle_angle(a: float, b: float, c: float) -> np.array:
         1. a, b, c (float): side length, 
     
     Return:
-        corresponding angles: A, B, C (np.array)
+        corresponding angles: A, B, C (np.ndarray)
     """
 
     cos_theta = (a**2 + b**2 - c**2) / (2*a*b)
     return np.arccos(cos_theta)
 
-def lines_intersection(P1: np.array, P2: np.array, P3: np.array, P4: np.array) -> np.array:
+def lines_intersection(
+    P1: np.ndarray,
+    P2: np.ndarray,
+    P3: np.ndarray,
+    P4: np.ndarray
+) -> np.ndarray:
     """
     extract the line-line intersection for two lines [P1, P2] and [P3, P4]
+    in two dimensions
 
     Inputs:
-        1. P1 (np.array): one point on line 1
-        2. P2 (np.array): another point on line 1
-        3. P3 (np.array): one point on line 2
-        4. P4 (np.array): another point on line 2
+        1. P1 (np.ndarray): one point on line 1
+        2. P2 (np.ndarray): another point on line 1
+        3. P3 (np.ndarray): one point on line 2
+        4. P4 (np.ndarray): another point on line 2
 
     Return:
-        intersection point (np.array)
+        intersection point (np.ndarray)
     """
-    x1, y1 = P1  #
-    x2, y2 = P2  #another point on line 1
-    x3, y3 = P3  #one point on line 2
-    x4, y4 = P4  #another point on line 2
+    x1, y1 = P1  # one point on line 1
+    x2, y2 = P2  # another point on line 1
+    x3, y3 = P3  # one point on line 2
+    x4, y4 = P4  # another point on line 2
 
     D = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
     Px = (x1*y2-y1*x2)*(x3-x4) - (x1-x2)*(x3*y4-y3*x4)
-    Px /= D 
+    Px /= D
     Py = (x1*y2-y1*x2)*(y3-y4) - (y1-y2)*(x3*y4-y3*x4)
-    Py /= D 
-    return np.array([Px, Py])
+    Py /= D
+    return np.ndarray([Px, Py])
 
-def LineWithinSquare(P1: np.array, P2: np.array, P3: np.array, P4: np.array,
-                     R0: np.array, vector: np.array) -> np.array:
+def LineWithinSquare(
+    P1: np.ndarray,
+    P2: np.ndarray,
+    P3: np.ndarray,
+    P4: np.ndarray,
+    R0: np.ndarray,
+    vector: np.ndarray
+) -> np.ndarray:
     """
-    calculate the line segment within a square defined by [P1, P2, P3, P4] in anti-clockwise manner
+    calculate the line segment within a square defined by [P1, P2, P3, P4] 
+    in anti-clockwise manner
 
     Inputs:
-        1. P1 (np.array): first point within a square
-        2. P2 (np.array): second point within a square
-        3. P3 (np.array): third point within a square
-        4. P4 (np.array): fourth point within a square
-        5. R0 (np.array): point within the sqaure
-        6. vector (np.array): pointing to R0 from R1 outside the square
+        1. P1 (np.ndarray): first point within a square
+        2. P2 (np.ndarray): second point within a square
+        3. P3 (np.ndarray): third point within a square
+        4. P4 (np.ndarray): fourth point within a square
+        5. R0 (np.ndarray): point within the sqaure
+        6. vector (np.ndarray): pointing to R0 from R1 outside the square
     
     Return:
-        line segment (np.array)
+        line segment (np.ndarray)
     """
 
     R1 = R0 - vector
     theta = np.arctan2(-vector[1], -vector[0])
 
-    #threshold angles from R0 to P's 
+    #threshold angles from R0 to P's
     points = np.zeros((4, 2))
-    points[0, :] = P1 
-    points[1, :] = P2 
-    points[2, :] = P3 
+    points[0, :] = P1
+    points[1, :] = P2
+    points[2, :] = P3
     points[3, :] = P4
     RtoP = points - R0
     angles = np.arctan2(RtoP[:, 1], RtoP[:, 0])
-    
+
     if (theta>angles[0]) and (theta<=angles[1]):
         return lines_intersection(P1, P2, R0, R1)
     elif (theta>angles[1]) and (theta<=angles[2]):
@@ -115,34 +137,3 @@ def LineWithinSquare(P1: np.array, P2: np.array, P3: np.array, P4: np.array,
         return lines_intersection(P3, P4, R0, R1)
     else:
         return lines_intersection(P4, P1, R0, R1)
-
-def Kronecker(i, j):
-    if i==j:
-        return 1 
-    else:
-        return 0
-
-def MomentOfInertia(positions: np.array, m: int=1, matrix: bool=False) -> np.array:
-    """
-    moment of inertia for a rigid body made of n points
-
-    Inputs:
-        1. positions (np.array): positions of the point particles as [numofatoms, 3]
-        2. m (int): assuming each point mass is 1.0/numofatoms
-        3. matrix (bool): return the results as a matrix of just [ixx iyy izz ixy ixz iyz]
-    
-    Return:
-        moment of inertia (np.array)
-    """
-
-    Iij = np.zeros((3, 3))
-    distance2 = np.square(positions).sum(axis=1)
-    for i in range(3):
-        for j in range(3):
-            Iij[i, j] = m*(distance2*Kronecker(i, j)-positions[:, i]*positions[:, j]).sum()
-    
-    Iij /= positions.shape[0]
-    if matrix:
-        return Iij
-    else:
-        return np.array([Iij[0,0], Iij[1,1], Iij[2,2], Iij[0,1], Iij[0,2], Iij[1,2]])
