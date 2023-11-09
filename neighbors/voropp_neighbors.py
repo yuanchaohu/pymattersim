3edfcd775da95db213c831cbb037e2ebbfdc4f88
jiangyou <youjiang97@gmail.com>
2023-11-09 10:45:47 +0800

    voropp naming

--- neighbors/voropp_neighbors.py
+++ neighbors/voropp_neighbors.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 
 from reader.reader_utils import Snapshots
-from utils.logging_utils import get_logger_handle
+from utils.logging import get_logger_handle
 
 logger = get_logger_handle(__name__)
 
