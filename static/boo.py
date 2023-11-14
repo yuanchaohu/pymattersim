c3671345ce560ce2e9a505d3d295835551add6ff
yuanchaohu <ychu0213@gmail.com>
2023-11-13 21:28:16 -0700

    correct nominator for time corr boo 3d

--- static/boo.py
+++ static/boo.py
@@ -369,7 +369,8 @@ class boo_3d:
         )
 
         # normalization
-        gl_time["time_corr"] *= 4*np.pi/(self.l+1)/gl_time.loc[0, "time_corr"]
+        gl_time["time_corr"] *= 4*np.pi/(2*self.l+1)
+        gl_time["time_corr"] /= gl_time.loc[0, "time_corr"]
         if outputfile:
             gl_time.to_csv(outputfile, float_format="%.6f", index=False)
         return gl_time
