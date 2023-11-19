3a351ae3ec48e3d02e7709c06de531c849f49c9b
yuanchaohu <ychu0213@gmail.com>
2023-11-19 09:21:44 -0700

    update time average function in coarse-graining

--- utils/coarse_graining.py
+++ utils/coarse_graining.py
@@ -4,43 +4,48 @@
 see documentation @ ../docs/utils.md
 """
 
+from typing import Tuple
 import numpy as np
 from reader.reader_utils import Snapshots
+from utils.logging import get_logger_handle
+
+logger = get_logger_handle(__name__)
 
 def time_average(
-        raw_props: np.ndarray,
         snapshots: Snapshots,
-        avet: float=0.0,
+        input_property: np.ndarray,
+        time_period: float=0.0,
         dt: float=0.002
-) -> np.ndarray:
+) -> Tuple[np.ndarray, np.ndarray]:
     """
-    Calculate the time averaged input results
+    Calculate time average of the input property
 
     Input:
-        1. raw_props (np.ndarray): the raw property of particle,
-                                   in np.ndarray with shape [nsnapshots, nparticle]
-        3. avet (float): time used to average, default 0.0
+        1. snapshots
+        1. input_parameters (np.ndarray): the input particle-level property,
+                            in np.ndarray with shape [nsnapshots, nparticle]
+        3. time_period (float): time used to average, default 0.0
         4. dt (float): timestep used in user simulations, default 0.002
+
     Return:
-        Calculated time averaged input results (np.ndarray)
+        1. Calculated time averaged input results (np.ndarray)
+           shape [nsnapshots_updated, nparticles]
+        2. Corresponding snapshot id of the middle snapshot of each time period
+           shape [nsnapshots_updated]
     """
+    logger(f"Time average of the input variables for time_period={time_period}")
+    time_interval = snapshots.snapshots[1].timestep - snapshots.snapshots[0].timestep
+    time_interval *= dt
+    time_nsnapshot = int(time_period/time_interval)
+    # save the time averaged results
+    results = np.zeros((snapshots.nsnapshots-time_nsnapshot, snapshots.snapshots[0].nparticle))
+    # save the middle snapshot id for each time average period
+    results_middle_snapshots = []
 
-    timestep_interval = np.diff([snapshot.timestep for snapshot in snapshots.snapshots])[0]
-    assert len(set(np.diff(
-        [snapshot.timestep for snapshot in snapshots.snapshots]
-    ))) == 1, "Warning: Dump interval changes during simulation"
-
-    nparticle = snapshots.snapshots[0].nparticle
-    assert len(
-        {snapshot.nparticle for snapshot in snapshots.snapshots}
-    ) == 1, "Paticle number changes during simulation"
-
-    avet = int(avet/dt/timestep_interval)
-    averesults = np.zeros((snapshots.nsnapshots-avet, nparticle))
-
-    for n in range(snapshots.nsnapshots-avet):
-        averesults[n] = raw_props[n:n+avet].mean(axis=0)
-    return averesults
+    for n in range(results.shape[0]):
+        results[n, :] = input_property[n: n+time_nsnapshot].mean(axis=0)
+        results_middle_snapshots.append(round(n+time_nsnapshot/2))
+    return results, np.array(results_middle_snapshots)
 
 def spatial_average():
     pass
