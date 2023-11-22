67ee65cafe88c1eedbed5a805f87a5f02cb4b1c2
jiangyou <youjiang97@gmail.com>
2023-11-22 09:21:45 +0800

    add unit test for tetrahedral

--- shell/test_static.sh
+++ shell/test_static.sh
@@ -1,1 +1,2 @@
-python -m unittest discover tests/static -p '*_test.py' &> logs/test_static.log
+#python -m unittest discover tests/static -p '*_test.py' &> logs/test_static.log
+python -m unittest discover tests/static -p '*tetrahedral_test.py' &> logs/test_static.log
