4558472732a48ef32ce6b44bd601dc80e82d5c40
yuanchaohu <ychu0213@gmail.com>
2023-12-07 10:17:00 +0800

    pair entropy functions update

--- static/pairentropy.py
+++ static/pairentropy.py
@@ -1,15 +1,13 @@
 # coding = utf-8
 
-"""see documentation @ ../docs/pair_entropy.md"""
+"""see documentation @ ../docs/orderings.md"""
 
 import numpy as np
-import numexpr as ne
-from math import pi
 import pandas as pd
 from reader.reader_utils import Snapshots
 from utils.pbc import remove_pbc
 from utils.coarse_graining import time_average
-from utils.funcs import nidealfac, areafac, gaussian_smooth
+from utils.funcs import gaussian_smooth
 from utils.logging import get_logger_handle
 from static.gr import conditional_gr
 
@@ -19,6 +17,8 @@ logger = get_logger_handle(__name__)
 # pylint: disable=line-too-long
 
 def s2_integral(gr: np.ndarray, gr_bins: np.ndarray)->float:
+    """spatial integration of derivative g(r) product to get S2"""
+
     y = (gr * np.log(gr) - gr + 1) * np.square(gr_bins)
     return np.trapz(y, gr_bins)
 
@@ -27,27 +27,28 @@ class S2:
     """
     Calculate pair entropy S2 by calculating particle g(r) at single snapshot and integral to get S2,
     and then average S2 over desired time scales.
-    The code accounts for both orthogonal and triclinic cells, covering unary to quinary systems.
+    The code accounts for both orthogonal and triclinic cells
     """
     def __init__(
             self,
             snapshots: Snapshots,
             sigmas: np.ndarray,
             ppp: np.ndarray=np.array([1,1,1]),
-            rdelta: float=0.01,
-            rmax: int=10,
-            outputfile: str=None
+            rdelta: float=0.02,
+            ndelta: int=500,
     ) -> None:
         """
-        Initializing S2 class
+        Initializing S2 instance
 
         Inputs:
             1. snapshots (reader.reader_utils.Snapshots): snapshot object of input trajectory 
                          (returned by reader.dump_reader.DumpReader)
-            2. ppp (np.ndarray): the periodic boundary conditions,
+            2. sigmas: gaussian standard deviation for each pair particle type
+            3. ppp (np.ndarray): the periodic boundary conditions,
                                  setting 1 for yes and 0 for no, default np.ndarray=np.array([1,1,1]),
                                  set np.ndarray=np.array([1,1]) for two-dimensional systems
-            3. rdelta (float): bin size calculating g(r), the default value is 0.01
+            4. rdelta (float): bin size calculating g(r), the default value is 0.02
+            5. ndelta (int): number of bins for g(r) calculation, ndelta*rdelta determines the range
             4. outputfile (str): the name of csv file to save the calculated S2
 
         Return:
@@ -57,11 +58,8 @@ class S2:
         self.sigmas = sigmas
         self.ppp = ppp
         self.rdelta = rdelta
-        self.rmax = rmax
-        self.outputfile = outputfile
+        self.ndelta = ndelta
 
-        self.nsnapshots = self.snapshots.nsnapshots
-        self.ndim = self.snapshots.snapshots[0].positions.shape[1]
         self.nparticle = snapshots.snapshots[0].nparticle
         assert len({snapshot.nparticle for snapshot in self.snapshots.snapshots}) == 1,\
             "Paticle Number Changes during simulation"
@@ -74,106 +72,85 @@ class S2:
             "Sum of Indivdual Types is Not the Total Amount"
 
         self.rhototal = self.nparticle / self.boxvolume
-        self.rhotype = self.typecount / self.boxvolume
+        self.s2_results = 0
 
-        self.s2_results = np.zeros((self.nsnapshots, self.nparticle))
+    def particle_s2(self, savegr:bool=False, outputfile:str=None) -> np.ndarray:
+        """
+        Calculate the particle-level g(r) by Gaussian smoothing
+        and then calculate the particle level S2
 
-    def particle_s2(self):
+        Inputs:
+
+        Return:
+            particle level S2 in shape [nsnapshot, nparticle]
         """
-        calculate the particle-level g(r) by Gaussian smoothing
-        """
-        gr_bins = np.arange(0, self.rmax, self.rdelta)[1:] - 0.5*self.rdelta
-        
+
+        s2_results = np.zeros((self.snapshots.nsnapshots, self.nparticle))
+        gr_bins = np.arange(self.ndelta)*self.rdelta + self.rdelta/2
+        rmax = gr_bins.max()
+        if savegr:
+            particle_gr = np.zeros((self.snapshots.nsnapshots, self.nparticle, self.ndelta))
+
         if self.ppp.shape[0]==2:
-            norms = 2*np.pi*gr_bins # 2d
+            norms = 2*gr_bins*self.rhototal*np.pi
+        elif self.ppp.shape[0]==3:
+            norms = 4*np.square(gr_bins)*self.rhototal*np.pi
         else:
-            norms = 4*np.pi*np.square(gr_bins) # 3d
+            raise ValueError("Input dimension is not 2 or 3")
 
         for n, snapshot in enumerate(self.snapshots.snapshots):
-            for i in range(self.nparticle):
+            for i in range(snapshot.nparticle):
                 RIJ = np.delete(snapshot.positions, i, axis=0) - snapshot.positions[i]
                 RIJ = remove_pbc(RIJ, snapshot.hmatrix, self.ppp)
                 distance = np.linalg.norm(RIJ, axis=1)
-                distance = distance[distance<self.rmax]
+                distance = distance[distance<rmax]
                 itype = int(snapshot.particle_type[i]-1)
                 jtypes = (np.delete(snapshot.particle_type, i)-1).astype(np.int64)
                 gr_i = 0
                 for j, rij in enumerate(distance):
                     sigma = self.sigmas[itype, jtypes[j]]
                     gr_i += gaussian_smooth(gr_bins, rij, sigma)
-                gr_i /= (self.rhotype[itype] * norms)
-                self.s2_results[n, i] = -2 * np.pi * self.rhotype[itype] * s2_integral(gr_i, gr_bins)
-        return self.s2_results
+                gr_i /= norms
 
-    def timeave(
-        self,
-        time_period: float=0.0,
-        dt: float=0.002,
-        outputS2: str=None
-    ) -> np.ndarray:
-        """
-        Calculate Time Averaged S2 or instantaneous ones
+                if savegr:
+                    particle_gr[n, i, :] = gr_i
 
-        Inputs:
-            1. time_period (float): time used to average, default 0.0
-            2. dt (float): timestep used in user simulations, default 0.002
-            3. outputS2 (str): file name for S2 results, default None
-                               To reduce storage size and ensure loading speed, save npy file as default with extension ".npy".
-                               If the file extension is ".dat" or ".txt", it is saved as a test file.
-        
-        Return:
-            Calculated time averaged S2 (np.ndarray)
-            shape [nsnapshots_updated, nparticles]
-        """
-        if len(self.typenumber) == 1:
-            S2results = self.unary()
-        if len(self.typenumber) == 2:
-            S2results = self.binary()
-        if len(self.typenumber) == 3:
-            S2results = self.ternary()
-        if len(self.typenumber) == 4:
-            S2results = self.quarternary()
-        if len(self.typenumber) == 5:
-            S2results = self.quinary()
-        if len(self.typenumber) > 5:
-            S2results = self.unary()
-
-        results = time_average(snapshots=self.snapshots, input_property=S2results, time_period=time_period, dt=dt)[0]
-
-        if outputS2:
-            if outputS2[-3:] == 'npy':
-                np.save(outputS2, results)
-            elif outputS2[-3:] == ('dat' or 'txt'):
-                np.savetxt(outputS2, results, fmt='%.6f', header="", comments="")
-            else:
-                logger.info('The default format of outputfile is binary npy with extension "npy" and also supports text file with extension "dat" or "txt"')
-        return results
+                s2_results[n, i] = -2*np.pi*self.rhototal*s2_integral(gr_i, gr_bins)
+        self.s2_results = s2_results
+        if outputfile:
+            np.save(outputfile, s2_results)
+        if savegr:
+            np.save('particle_gr.'+outputfile, particle_gr)
+            return s2_results, particle_gr
+        return s2_results
 
     def spatial_corr(
         self,
-        timeaveS2: np.ndarray,
         outputfile: str=None
     ) -> pd.DataFrame:
         """
-        Calculate Spatial Correlation of Time Averaged or individual S2
+        Calculate Spatial Correlation of S2
 
         Inputs:
-            1. timeaveS2 (np.ndarray): calculated time averaged S2
-            2. outputfile (str): csv file name for gl, default None
+            1. outputfile (str): csv file name for gl, default None
+
+        Return
         """
         glresults = 0
-        for n, snapshot in enumerate(self.snapshots.snapshots[:timeaveS2.shape[0]]):
+        for _, snapshot in enumerate(self.snapshots):
             glresults += conditional_gr(
                 snapshot=snapshot,
-                condition=timeaveS2[n],
+                condition=self.s2_results,
                 conditiontype=None,
                 ppp=self.ppp,
                 rdelta=self.rdelta
             )
-        glresults /= self.nsnapshots
+        glresults /= self.snapshots.nsnapshots
         if outputfile:
             glresults.to_csv(outputfile, float_format="%.8f", index=False)
 
         logger.info('Finish calculating spatial correlation of S2')
         return glresults
 
+    def time_corr(self):
+        pass
