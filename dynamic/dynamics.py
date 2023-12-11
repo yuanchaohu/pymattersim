94a5bd4394eaaf0f6ad494953060b25860b17d65
yuanchaohu <ychu0213@gmail.com>
2023-12-11 17:39:08 +0800

    update absolute and cage-relative dynamics functions

--- dynamic/dynamics.py
+++ dynamic/dynamics.py
@@ -35,7 +35,8 @@ class dynamics:
         xu_snapshots: Snapshots=None,
         x_snapshots: Snapshots=None,
         dt: float=0.002,
-        ppp: np.ndarray=np.array([0,0,0])
+        ppp: np.ndarray=np.array([0,0,0]),
+        neighborfile: str=None
     ) -> None:
         """
         Initializing dynamics class
@@ -53,53 +54,41 @@ class dynamics:
         Return:
             None
         """
-        logger.info('True coordinates by xu are suggested for dynamics calculation')
-
+        self.ppp = ppp
+        self.neighborfile = neighborfile
         self.ndim = len(ppp)
-        self.dt = dt
+        if self.ndim==2 and self.neighborfile:
+            logger.info("Computer cage-relative dynamics for two-dimensional systems")
 
         if x_snapshots and xu_snapshots:
             logger.info('Use both x and xu coordinates to calculate dynamics')
-
-            self.ppp = np.array([0,0,0])[:self.ndim]
-            self.PBC = False
-            self.hmatrix = None
-            self.positions = [snapshot.positions for snapshot in xu_snapshots.snapshots] # calculate dynamics
-            self.original = [snapshot.positions for snapshot in x_snapshots.snapshots]   # calculate S4
+            self.snapshots = xu_snapshots
+            self.x_snapshots = x_snapshots
             if xu_snapshots.nsnapshots != x_snapshots.nsnapshots:
                 raise ValueError('----incompatible x and xu format coordinates----')
-            self.snapshots = x_snapshots
-            self.xu_snapshots = xu_snapshots
         elif xu_snapshots and not x_snapshots:
             logger.info('Use xu coordinates to calculate dynamics')
-            self.ppp = np.array([0,0,0])[:self.ndim]
-            self.PBC = False
-            self.hmatrix = None
-            self.positions = [snapshot.positions for snapshot in xu_snapshots.snapshots]
-            self.original = self.positions.copy()
             self.snapshots = xu_snapshots
         elif x_snapshots and not xu_snapshots:
             logger.info('Use x coordinates to calculate dynamics')
-            self.ppp = ppp[:self.ndim]
-            self.PBC = True
-            self.hmatrix = [snapshot.hmatrix for snapshot in x_snapshots.snapshots]
-            self.positions = [snapshot.positions for snapshot in x_snapshots.snapshots]
-            self.original = self.positions.copy()
             self.snapshots = x_snapshots
-
-        self.timestep = set(np.diff([snapshot.timestep for snapshot in self.snapshots.snapshots])).pop()
-        self.nparticle = set([snapshot.nparticle for snapshot in self.snapshots.snapshots]).pop()
-        self.particle_type = self.snapshots.snapshots[0].particle_type
-        self.nsnapshots = self.snapshots.nsnapshots
-        self.boxlength = self.snapshots.snapshots[0].boxlength
-        self.typenumber, self.typecount = np.unique(self.particle_type, return_counts=True)
+        else:
+            logger.info("Please provide ")
         
-    def total(
+        self.time = [snapshot.timestep for snapshot in self.snapshots.snapshots]
+        self.time = (np.array(self.time[1:]) - self.time[0])*dt
+
+    def total(self, outputfile):
+        if self.neighborfile:
+            self.results, self.particle_msd = self.cagerelative()
+        else:
+           self.results, self.particle_msd = self.absolute()
+
+    def absolute(
         self,
-        qmax: float,
-        a: float=1.0,
-        outputfile: str=None
-    ) -> np.ndarray:
+        qmax: float=6.28,
+        a: float=0.3
+    ) -> pd.DataFrame:
         """
         Compute self-intermediate scattering functions ISF, dynamic susceptibility ISFX4 based on ISF
         Overlap function Qt and its corresponding dynamic susceptibility QtX4
@@ -113,119 +102,99 @@ class dynamics:
         Return:
             Calculated dynamics results in np.ndarray
         """
-        logger.info(f'Start calculating dynamics in {self.ndim} dimensionality system')
+        logger.info(f'Start calculating abosulte dynamics in {self.ndim} dimensional system')
+        a2 = a**2
+        counts = np.zeros(self.snapshots.nsnapshots-1)
+        isf = np.zeros_like(counts)
+        isf2 = np.zeros_like(counts)
+        qt = np.zeros_like(counts)
+        qt2 = np.zeros_like(counts)
+        r2 = np.zeros_like(counts)
+        r4 = np.zeros_like(counts)
+        particle_msd = np.zeros((self.snapshots.nsnapshots, self.snapshots.snapshots[0].nparticle))
+        for n in range(self.snapshots.nsnapshots):
+            for nn in range(1, n+1):
+                counts[nn] += 1
+                RII = self.snapshots.positions[n] - self.snapshots.positions[n-nn]
+                RII = remove_pbc(RII, self.snapshots.hmatrix[n-nn], self.ppp)
+                # self-intermediate scattering function
+                medium = np.cos(RII*qmax).mean(axis=1).sum()
+                isf[nn] += medium
+                isf2[nn] += medium**2
+                # overlap function
+                distance = np.square(RII).sum(axis=1)
+                medium =( distance < a2).sum()
+                qt[nn] += medium
+                qt2[nn] += medium**2
+                # mean-squared displacements & non-gaussian parameter
+                particle_msd[nn, :] = distance
+                r2[nn] += distance.sum()
+                r4[nn] += np.square(distance).sum()
+        isf /= counts
+        isf2 /= counts
+        x4_isf = np.square(isf) - isf2
 
-        results = np.zeros(((self.nsnapshots-1), 7))
-        names = 't  ISF  ISFX4  Qt  QtX4  msd  alpha2'
-        
-        cal_isf = pd.DataFrame(np.zeros((self.nsnapshots-1))[np.newaxis, :])
-        cal_Qt = pd.DataFrame(np.zeros((self.nsnapshots-1))[np.newaxis, :])
-        cal_msd = pd.DataFrame(np.zeros((self.nsnapshots-1))[np.newaxis, :])
-        cal_alp = pd.DataFrame(np.zeros((self.nsnapshots-1))[np.newaxis, :])
-        deltat = np.zeros(((self.nsnapshots-1), 2), dtype=np.int32) #deltat, deltatcounts
-        for n in range(self.nsnapshots-1):
-            RII = self.positions[n+1:] - self.positions[n]
-            if self.PBC:
-                for ii in range(len(RII)):
-                    RII[ii] = remove_pbc(RII[ii], self.hmatrix[n], self.ppp)
+        qt /= counts
+        qt2 /= counts
+        x4_qt = np.square(qt) - qt2
 
-            RII_isf = (np.cos(RII*qmax).mean(axis=2)).sum(axis=1) #index is timeinterval -1
-            cal_isf = pd.concat([cal_isf, pd.DataFrame(RII_isf[np.newaxis, :])])
-            distance = np.square(RII).sum(axis=2)
-            RII_Qt = (distance<=a**2).sum(axis=1)
-            cal_Qt = pd.concat([cal_Qt, pd.DataFrame(RII_Qt[np.newaxis, :])])
-            cal_msd = pd.concat([cal_msd, pd.DataFrame(distance.sum(axis = 1)[np.newaxis, :])])
-            distance2 = np.square(distance).sum(axis=1)
-            cal_alp = pd.concat([cal_alp, pd.DataFrame(distance2[np.newaxis, :])])
-       
-        cal_isf = cal_isf.iloc[1:]
-        cal_Qt = cal_Qt.iloc[1:]
-        cal_msd = cal_msd.iloc[1:]
-        cal_alp = cal_alp.iloc[1:]
-        deltat[:, 0] = np.array(cal_isf.columns) + 1 # Timeinterval
-        deltat[:, 1] = np.array(cal_isf.count())     # Timeinterval frequency
+        alpha2 = alpha2factor(self.ndim) * r2/r4 - 1
+        results = np.column_stack((isf, x4_isf, qt, x4_qt, r2, alpha2))
+        results /= self.snapshots.snapshots[0].nparticle
+        results = np.column_stack((self.time, results))
+        results = pd.DataFrame(results, columns='t isf X4_isf Qt X4_Qt msd alpha2'.split())
+        return results, particle_msd
 
-        results[:, 0] = deltat[:, 0] * self.timestep * self.dt 
-        results[:, 1] = cal_isf.mean() / self.nparticle
-        results[:, 2] = ((cal_isf**2).mean() - (cal_isf.mean())**2) / self.nparticle
-        results[:, 3] = cal_Qt.mean() / self.nparticle
-        results[:, 4] = ((cal_Qt**2).mean() - (cal_Qt.mean())**2) / self.nparticle
-        results[:, 5] = cal_msd.mean() / self.nparticle
-        results[:, 6] = cal_alp.mean() / self.nparticle
-        results[:, 6] = alpha2factor(self.ndim) * results[:, 6] / np.square(results[:, 5]) - 1.0
+    def cagerelative(self):
+        # step 1: read neighbors to a list
+        # step 2: calculate cag
+        logger.info(f'Start calculating cage-relative dynamics in {self.ndim} dimensional system')
+        a2 = a**2
+        counts = np.zeros(self.snapshots.nsnapshots-1)
+        isf = np.zeros_like(counts)
+        isf2 = np.zeros_like(counts)
+        qt = np.zeros_like(counts)
+        qt2 = np.zeros_like(counts)
+        r2 = np.zeros_like(counts)
+        r4 = np.zeros_like(counts)
+        for n in range(self.snapshots.nsnapshots):
+            for nn in range(1, n+1):
+                counts[nn] += 1
+                RII = self.snapshots.positions[n] - self.snapshots.positions[n-nn]
+                RII = remove_pbc(RII, self.snapshots.hmatrix[n-nn], self.ppp)
+                # self-intermediate scattering function
+                medium = np.cos(RII*qmax).mean(axis=1).sum()
+                isf[nn] += medium
+                isf2[nn] += medium**2
+                # overlap function
+                distance = np.square(RII).sum(axis=1)
+                medium =( distance < a2).sum()
+                qt[nn] += medium
+                qt2[nn] += medium**2
+                # mean-squared displacements & non-gaussian parameter
+                r2[nn] += distance.sum()
+                r4[nn] += np.square(distance).sum()
+        isf /= counts
+        isf2 /= counts
+        x4_isf = np.square(isf) - isf2
 
-        if outputfile:
-            np.savetxt(outputfile, results, fmt='%.8f', header = names, comments = '')
-        logger.info(f'Finish calculating dynamics in {self.ndim} dimensionality system')
+        qt /= counts
+        qt2 /= counts
+        x4_qt = np.square(qt) - qt2
+
+        alpha2 = alpha2factor(self.ndim) * r2/r4 - 1
+        results = np.column_stack((isf, x4_isf, qt, x4_qt, r2, alpha2))
+        results /= self.snapshots.snapshots[0].nparticle
+        results = np.column_stack((self.time, results))
+        results = pd.DataFrame(results, columns='t isf X4_isf Qt X4_Qt msd alpha2'.split())
         return results
 
-    def partial(self, qmax=[], a=[1.0, 1.0], outputfile=''):
-        """ Compute self-intermediate scattering functions ISF, dynamic susceptibility ISFX4 based on ISF
-            Overlap function Qt and its corresponding dynamic susceptibility QtX4
-            Mean-square displacements msd; non-Gaussion parameter alpha2
-        
-            qmax is the wavenumber corresponding to the first peak of structure factor
-            qmax accounts for six components so it is a list, input as a list for each particle type
-            a is the cutoff for the overlap function, default is 1.0 (EAM) and 0.3(LJ) (0.3<d>), input as a list for each type
-        """
-        print ('----Compute Partial Dynamics----')
 
-        partialresults = [] #a list containing results of all particle types           
-        for i in self.Type:  #loop over different particle types
-            #TYPESET = np.where(np.array(self.ParticleType) == i, 1, 0).astype(np.int)
-            TYPESET = [j == i for j in self.ParticleType]
+    def conditional(self, conditions):
+        pass
 
-            results = np.zeros(((self.SnapshotNumber - 1), 7))
-            names  = 't  ISF  ISFX4  Qt  QtX4  msd  alpha2'
-            
-            cal_isf  = pd.DataFrame(np.zeros((self.SnapshotNumber-1))[np.newaxis, :])
-            cal_Qt   = pd.DataFrame(np.zeros((self.SnapshotNumber-1))[np.newaxis, :])
-            cal_msd  = pd.DataFrame(np.zeros((self.SnapshotNumber-1))[np.newaxis, :])
-            cal_alp  = pd.DataFrame(np.zeros((self.SnapshotNumber-1))[np.newaxis, :])
-            deltat   = np.zeros(((self.SnapshotNumber - 1), 2), dtype = np.int) #deltat, deltatcounts
-            for n in range(self.SnapshotNumber - 1):  #loop over time intervals
-                #RII    = self.Positions[n + 1:] - self.Positions[n]
-                RII  = [ii[TYPESET[n]] - self.Positions[n][TYPESET[n]] for ii in self.Positions[n+1:]]
-                if self.PBC:
-                    hmatrixinv = np.linalg.inv(self.hmatrix[n])
-                    for ii in range(len(RII)):
-                        matrixij = np.dot(RII[ii], hmatrixinv)
-                        RII[ii]  = np.dot(matrixij - np.rint(matrixij) * self.ppp, self.hmatrix[n]) #remove PBC
-
-                RII       = np.array(RII)
-                RII_isf   = ((np.cos(RII * qmax[i - 1]).mean(axis = 2))).sum(axis = 1) #index is timeinterval -1
-                cal_isf   = pd.concat([cal_isf, pd.DataFrame(RII_isf[np.newaxis, :])])
-                distance  = np.square(RII).sum(axis = 2)
-                RII_Qt    = (distance <= a**2).sum(axis = 1)
-                cal_Qt    = pd.concat([cal_Qt, pd.DataFrame(RII_Qt[np.newaxis, :])])
-                cal_msd   = pd.concat([cal_msd, pd.DataFrame(distance.sum(axis = 1)[np.newaxis, :])])
-                distance2 = (np.square(distance)).sum(axis = 1)
-                cal_alp   = pd.concat([cal_alp, pd.DataFrame(distance2[np.newaxis, :])])
-        
-            cal_isf      = cal_isf.iloc[1:]
-            cal_Qt       = cal_Qt.iloc[1:]
-            cal_msd      = cal_msd.iloc[1:]
-            cal_alp      = cal_alp.iloc[1:]
-            deltat[:, 0] = np.array(cal_isf.columns) + 1 #Timeinterval
-            deltat[:, 1] = np.array(cal_isf.count())     #Timeinterval frequency
-
-            results[:, 0] = deltat[:, 0] * self.TimeStep * self.dt 
-            results[:, 1] = cal_isf.mean() / self.TypeNumber[i - 1]
-            results[:, 2] = ((cal_isf**2).mean() - (cal_isf.mean())**2) / self.TypeNumber[i - 1]
-            results[:, 3] = cal_Qt.mean() / self.TypeNumber[i - 1]
-            results[:, 4] = ((cal_Qt**2).mean() - (cal_Qt.mean())**2) / self.TypeNumber[i - 1]
-            results[:, 5] = cal_msd.mean() / self.TypeNumber[i - 1]
-            results[:, 6] = cal_alp.mean() / self.TypeNumber[i - 1]
-            results[:, 6] = alpha2factor(self.ndim) * results[:, 6] / np.square(results[:, 5]) - 1.0
-
-            if outputfile:
-                np.savetxt('Type' + str(i) + '.' + outputfile, results, fmt='%.8f', header = names, comments = '')
-            
-            partialresults.append(results)
-
-        print ('----Compute Partial Dynamics Over----')
-        return partialresults, names
-
+    def S4(self, conditions):
+        pass
     def slowS4(self, X4time, a=1.0, qrange=10, onlypositive=False, outputfile=''):
         """ Compute four-point dynamic structure factor of slow atoms at peak timescale of dynamic susceptibility
 
@@ -277,86 +246,3 @@ class dynamics:
         print ('----Compute S4(q) of slow particles over----')
         return results, names
 
-    def fastS4(self, a=1.0, X4timeset=0, qrange=10, onlypositive=False, outputfile=''):
-        """ Compute four-point dynamic structure factor for fast particles at peak timescale of dynamic susceptibility
-
-            Based on overlap function Qt and its corresponding dynamic susceptibility QtX4     
-            a is the cutoff for the overlap function, default is 1.0 (EAM) and 0.3(LJ) (0.3<d>)
-            X4timeset is the peaktime scale of X4, if 0 will use the calculated one
-            Dynamics should be calculated before computing S4
-            xu coordinates should be used to identify fast atoms by calculating Qt and X4
-            x  coordinates should be used to calculate FFT
-        """
-        print ('----Compute dynamic S4(q) of fast particles----')
-
-        #-----------calculte overall dynamics first----------------
-        results = np.zeros(((self.SnapshotNumber - 1), 3))
-        names  = 't  Qt  QtX4'
-        
-        cal_Qt   = pd.DataFrame(np.zeros((self.SnapshotNumber-1))[np.newaxis, :])
-        deltat   = np.zeros(((self.SnapshotNumber - 1), 2), dtype = np.int) #deltat, deltatcounts
-        for n in range(self.SnapshotNumber - 1):  #time interval
-            RII = self.Positions[n + 1:] - self.Positions[n]
-            if self.PBC:
-                hmatrixinv = np.linalg.inv(self.hmatrix[n])
-                for ii in range(len(RII)):
-                    matrixij = np.dot(RII[ii], hmatrixinv)
-                    RII[ii]  = np.dot(matrixij - np.rint(matrixij) * self.ppp, self.hmatrix[n]) #remove PBC
-
-            distance  = np.square(RII).sum(axis = 2)
-            RII_Qt    = (distance >= a**2).sum(axis = 1)
-            cal_Qt    = pd.concat([cal_Qt, pd.DataFrame(RII_Qt[np.newaxis, :])])
-        
-        cal_Qt       = cal_Qt.iloc[1:]
-        deltat[:, 0] = np.array(cal_Qt.columns) + 1 #Timeinterval
-        deltat[:, 1] = np.array(cal_Qt.count())     #Timeinterval frequency
-
-        results[:, 0] = deltat[:, 0] * self.TimeStep * self.dt 
-        results[:, 1] = cal_Qt.mean() / self.ParticleNumber
-        results[:, 2] = ((cal_Qt**2).mean() - (cal_Qt.mean())**2) / self.ParticleNumber
-        if outputfile:
-            np.savetxt('Dynamics.' + outputfile, results, fmt='%.8f', header = names, comments = '')
-
-        #-----------calculte S4(q) of fast particles----------------
-        twopidl = 2 * pi / self.Boxlength
-        Numofq = int(qrange*2.0 / twopidl.min())
-        qvector = choosewavevector(self.ndim, Numofq, onlypositive)
-        qvector = qvector.astype(np.float64) * twopidl[np.newaxis, :]
-        qvalues = np.linalg.norm(qvector, axis=1)
-
-        sqresults = np.zeros((qvector.shape[0], 2))
-        sqresults[:, 0] = qvalues
-
-        if X4timeset:
-            X4time = int(X4timeset / self.dt / self.TimeStep)
-        else:
-            X4time = deltat[results[:, 2].argmax(), 0] 
-
-        for n in range(self.SnapshotNumber - X4time):
-            RII = self.Positions[n + X4time] - self.Positions[n]
-            if self.PBC:
-                #hmatrixinv = np.linalg.inv(self.hmatrix[n])
-                #matrixij   = np.dot(RII, hmatrixinv)
-                #RII        = np.dot(matrixij - np.rint(matrixij) * self.ppp, self.hmatrix[n]) #remove PBC
-                RII = remove_PBC(RII, self.hmatrix[n], self.ppp)
-
-            RII = np.linalg.norm(RII, axis = 1)
-            RII = np.where(RII >= a, 1, 0)
-            
-            sqtotal = np.zeros_like(sqresults)
-            for i in range(self.ParticleNumber):
-                if RII[i]:
-                    thetas = (self.original[n][i][np.newaxis, :] * qvector).sum(axis=1)
-                    sqtotal[:, 0] += np.sin(thetas)
-                    sqtotal[:, 1] += np.cos(thetas)
-            
-            sqresults[:, 1] += np.square(sqtotal).sum(axis=1) / self.ParticleNumber
-        sqresults[:, 1] /= (self.SnapshotNumber - X4time)
-        
-        sqresults = pd.DataFrame(sqresults).round(8)
-        results = sqresults.groupby(sqresults[0]).mean().reset_index().values      
-        names = 'q  S4'
-        if outputfile:
-            np.savetxt(outputfile, results, fmt='%.8f', header = names, comments = '')
-        print ('----Compute S4(q) of fast particles over----')
-        return results, names
