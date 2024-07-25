b0915a05bb533d1fb9d1d2e4ff1189790a1759ca
yuanchaohu <ychu0213@gmail.com>
2024-07-25 13:25:50 +0800

    reformat Qtensor and add time_corr and spatial_corr for Q-tensor

--- static/nematic.py
+++ static/nematic.py
@@ -3,17 +3,18 @@
 """see documentation @ ../docs/orderings.md"""
 
 import numpy as np
+import pandas as pd
 from reader.reader_utils import Snapshots
+from static.gr import conditional_gr
+from dynamic.time_corr import time_correlation
 from utils.funcs import kronecker
 from utils.coarse_graining import spatial_average
 from utils.logging import get_logger_handle
 
 logger = get_logger_handle(__name__)
 
-# TODO @Yibang please benchmark with
-# https://github.com/yuanchaohu/MyCodes/blob/master/Nematic.py#L152
-# for both with neighborfile and without neighborfile
 class NematicOrder():
+    """calculate tensor order paramater and its correlation functions"""
     def __init__(
             self,
             snapshots_orientation: Snapshots,
@@ -23,13 +24,16 @@ class NematicOrder():
         Calculate the nematic order parameter of a model system
 
         Inputs:
-            1. snapshots_orientation (reader.reader_utils.Snapshots): snapshot object of input trajectory
+            1. snapshots_orientation (reader.reader_utils.Snapshots): 
+                                snapshot object of input trajectory
                                 (returned by reader.dump_reader.DumpReader)
                                 !!! DumpFileType=LAMMPSVECTOR
-            2. snapshots_position (reader.reader_utils.Snapshots): snapshot object of input trajectory
+            2. snapshots_position (reader.reader_utils.Snapshots): 
+                                snapshot object of input trajectory
                                 (returned by reader.dump_reader.DumpReader)
-                                !!! DumpFileType=LAMMPS or LAMMPSCENTER or any other to provide atom positions
-                                Not mandatory if only calculate nematic tensor
+                                !!! DumpFileType=LAMMPS or LAMMPSCENTER 
+                                or any other to provide atom positions
+                                Only required for spatial correlation calculation
 
         Return:
             None
@@ -38,6 +42,9 @@ class NematicOrder():
         self.snapshots = snapshots_position
         self.QIJ = 0
 
+    # TODO @Yibang please benchmark with
+    # https://github.com/yuanchaohu/MyCodes/blob/master/Nematic.py#L152
+    # for both with neighborfile and without neighborfile
     def tensor(
             self,
             ndim: int=2,
@@ -99,19 +106,63 @@ class NematicOrder():
                     eigenvalues[n, i] = np.linalg.eig(QIJ[n,i])[0].max()*2.0
             np.save(outputfile+".eigval.npy", eigenvalues)
             return eigenvalues
-        else:
-            Qtrace = np.zeros((QIJ.shape[0], QIJ.shape[1]))
-            for n in range(self.orientations.nsnapshots):
-                for i in range(self.orientations.snapshots[n].nparticle):
-                    Qtrace[n, i] = np.trace(np.matmul(QIJ[n,i], QIJ[n,i]))
-            Qtrace *= ndim / (ndim-1)
-            Qtrace = np.sqrt(Qtrace)
-            np.save(outputfile+".Qtrace.npy", Qtrace)
-            return Qtrace
-        logger.info("Calculate the Nematic order tensor and scalar parameters done")
+        # otherwise calculate the trace
+        Qtrace = np.zeros((QIJ.shape[0], QIJ.shape[1]))
+        for n in range(self.orientations.nsnapshots):
+            for i in range(self.orientations.snapshots[n].nparticle):
+                Qtrace[n, i] = np.trace(np.matmul(QIJ[n,i], QIJ[n,i]))
+        Qtrace *= ndim / (ndim-1)
+        Qtrace = np.sqrt(Qtrace)
+        np.save(outputfile+".Qtrace.npy", Qtrace)
+        return Qtrace
 
-    def spatial_corr(self):
-        pass
-    
-    def time_corr(self):
-        pass
+    # TODO @Yibang please benchmark with
+    # https://github.com/yuanchaohu/MyCodes/blob/master/Nematic.py#L223
+    def spatial_corr(self, rdelta:float=0.01, ppp:np.ndarray=np.array([1,1]), outputfile:str=""):
+        """ 
+        Calculate the spatial correlation of the nematic order QIJ
+
+        Inputs:
+            1. rdelta (float): bin size in calculating g(r) and G_Q(r), default 0.01
+            2. ppp (np.ndarray): 
+            2. outputfile (str): csv file name for G_Q(r), default None
+
+        Return:
+            calculated g_Q(r) based on QIJ tensor
+        """
+        logger.info("Calculate spatial correlation of tensorial QIJ")
+        gQresults = 0
+        for n, snapshot in enumerate(self.snapshots.snapshots):
+            gQresults += conditional_gr(
+                snapshot=snapshot,
+                condition=self.QIJ[n],
+                conditiontype="tensor",
+                ppp=ppp,
+                rdelta=rdelta
+            )
+        gQresults /= self.snapshots.nsnapshots
+        if outputfile:
+            gQresults.to_csv(outputfile, float_format="%.8f", index=False)
+        return gQresults
+
+    # TODO @Yibang please benchmark with
+    # https://github.com/yuanchaohu/MyCodes/blob/master/Nematic.py#L262
+    def time_corr(self, dt:float=0.002, outputfile:str="") -> pd.DataFrame:
+        """
+        Calculate time correlation of the tensorial nematic order parameter
+
+        Inputs:
+            1. dt (float): timestep used in user simulations, default 0.002
+            2. outputfile (str): csv file name for time correlation results, default None
+
+        Return:
+            time correlation quantity (pd.DataFrame)
+        """
+        logger.info('Calculate time correlation of tensorial QIJ')
+        gQ_time = time_correlation(
+            snapshots=self.orientations,
+            condition=self.QIJ,
+            dt=dt,
+            outputfile=outputfile
+        )
+        return gQ_time
