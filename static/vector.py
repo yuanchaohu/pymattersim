a474a19bea7d8a054b61574bc3f6253cd0a8d55d
yuanchaohu <ychu0213@gmail.com>
2024-07-25 16:41:33 +0800

    vector decompositon and its sq in kspace

--- static/vector.py
+++ static/vector.py
@@ -1,13 +1,16 @@
 """see documentation @ ../docs/vectors.md"""
 
+from enum import unique
 from typing import List, Any, Optional, Tuple
 import numpy as np
 import pandas as pd
 from math import pi
-from reader.reader_utils import Snapshots
+
+from sqlalchemy import values
+from reader.reader_utils import Snapshots, SingleSnapshot
 from neighbors.read_neighbors import read_neighbors
 from utils.pbc import remove_pbc
-from utils.wavevector import choosewavevector, continuousvector
+from sq import conditional_sq
 from utils.logging import get_logger_handle
 
 logger = get_logger_handle(__name__)
@@ -166,3 +169,51 @@ def vibrability(
     if outputfile:
         np.save(outputfile, results)
     return results
+
+# TODO @Yibang please benchmark with
+# https://github.com/yuanchaohu/MyCodes/blob/master/EigenvectorAnalysis.py#L391
+def vector_TL_sq(
+        snapshot: SingleSnapshot,
+        qvector: np.ndarray,
+        vector: np.ndarray,
+        outputfile: str="",
+    ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Decompose the input vector into transverse and longitudinal component
+    and calculate the associated magitude
+
+    Input:
+        1. snapshot (reader.reader_utils.SingleSnapshot): single snapshot object of input trajectory
+        2. qvector (np.ndarray of int): designed wavevectors in two-dimensional np.array 
+                                        (see utils.wavevector)
+        3. vector (np.ndarray): particle-level vector, shape as [num_of_particles, ndim],
+                                for example, eigenvector field and velocity field
+    
+    Return:
+        calculated transverse and longitudinal S(q) for each input wavevector (pd.DataFrame)
+        and the ensemble averaged S(q) over the same wavenumber (pd.DataFrame)
+        (FFT in complex number is also returned for reference)
+    """
+    ndim = qvector.shape[1]
+    vector_fft = conditional_sq(snapshot, qvector, vector)[0]
+    unitq = vector_fft[f"q{i}" for i in range(ndim)].values/vector_fft["q"].values
+    fft_columns = vector_fft[f"FFT{i}" for i in range(ndim)].values
+    vector_L = np.zeros_like(fft_columns)
+    for n in range(qvector.shape[0]):
+        medium = np.dot(unitq[n], fft_columns[n])
+        vector_L[n] = unitq[n] * medium
+    vector_T = fft_columns - vector_L
+
+    medium = pd.DataFrame(vector_T, columns=[f"T_FFT{i}" for i in range(ndim)])
+    vector_fft.join(medium)
+    vector_fft["Sq_T"] = (vector_T * np.conj(vector_T)).sum(axis=1).real
+
+    medium = pd.DataFrame(vector_L, columns=[f"L_FFT{i}" for i in range(ndim)])
+    vector_fft.join(medium)
+    vector_fft["Sq_L"] = (vector_L * np.conj(vector_L)).sum(axis=1).real
+
+    vector_fft = vector_fft.round(8)
+    ave_sqresults = vector_fft[["Sq", "Sq_T", "Sq_L"]].groupby(vector_fft["q"]).mean().reset_index()
+    if outputfile:
+        ave_sqresults.to_csv(outputfile, float_format="%.8f", index=False)
+    return vector_fft, ave_sqresults
