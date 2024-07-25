d4339208ab92434380343213e304fece55c56d9a
yuanchaohu <ychu0213@gmail.com>
2024-07-25 11:55:32 +0800

    make nematic order parameter as a class

--- static/nematic.py
+++ static/nematic.py
@@ -13,76 +13,105 @@ logger = get_logger_handle(__name__)
 # TODO @Yibang please benchmark with
 # https://github.com/yuanchaohu/MyCodes/blob/master/Nematic.py#L152
 # for both with neighborfile and without neighborfile
-def Qtensor(
-        Snapshots: Snapshots,
-        ndim: int=2,
-        neighborfile: str="",
-        Nmax: int=30,
-        eigvals: bool=False,
-        outputfile: str="",
-    ) -> np.ndarray:
-    """
-    Calculate the nematic order parameter of a model system
+class NematicOrder():
+    def __init__(
+            self,
+            snapshots_orientation: Snapshots,
+            snapshots_position: Snapshots=None,
+        ) -> None:
+        """
+        Calculate the nematic order parameter of a model system
 
-    Inputs:
-        1. Snapshots (reader.reader_utils.Snapshots): snapshot object of input trajectory
-                     (returned by reader.dump_reader.DumpReader)
-                     DumpFileType=LAMMPSVECTOR
-        2. ndim (int): dimensionality of the input configurations
-        3. neighborfile (str): file name of particle neighbors (see module neighbors)
-        4. Nmax (int): maximum number for neighbors, default 30
-        5. eigvals (bool): whether calculate eigenvalue of the Qtensor or not, default False
-        6. outputfile (str): file name of the calculation output
+        Inputs:
+            1. snapshots_orientation (reader.reader_utils.Snapshots): snapshot object of input trajectory
+                                (returned by reader.dump_reader.DumpReader)
+                                !!! DumpFileType=LAMMPSVECTOR
+            2. snapshots_position (reader.reader_utils.Snapshots): snapshot object of input trajectory
+                                (returned by reader.dump_reader.DumpReader)
+                                !!! DumpFileType=LAMMPS or LAMMPSCENTER or any other to provide atom positions
+                                Not mandatory if only calculate nematic tensor
+
+        Return:
+            None
+        """
+        self.orientations = snapshots_orientation
+        self.snapshots = snapshots_position
+        self.QIJ = 0
+
+    def tensor(
+            self,
+            ndim: int=2,
+            neighborfile: str="",
+            Nmax: int=30,
+            eigvals: bool=False,
+            outputfile: str="",
+        ) -> np.ndarray:
+        """
+        Calculate the nematic order parameter of a model system
+
+        Inputs:
+            1. ndim (int): dimensionality of the input configurations
+            2. neighborfile (str): file name of particle neighbors (see module neighbors)
+            3. Nmax (int): maximum number for neighbors, default 30
+            4. eigvals (bool): whether calculate eigenvalue of the Qtensor or not, default False
+            5. outputfile (str): file name of the calculation output
+        
+        Return:
+            Q-tensor or eigenvalue scalar nematic order parameter in numpy ndarray format
+            shape as [num_of_snapshots, num_of_particles]
+        """
+        if neighborfile:
+            logger.info(f"Calcualte coarse-grained nematic order for {self.orientations.nsnapshots} configurations")
+        else:
+            logger.info(f"Calcualte original nematic order for {self.orientations.nsnapshots} configurations")
+        # TODO add a function for three dimensioanl systems
+        assert ndim==2, "please set the correction dimensionality"
+
+        QIJ = []
+        for snapshot in self.orientations.snapshots:
+            medium = np.zeros((snapshot.nparticle, ndim, ndim))
+            for i in range(snapshot.nparticle):
+                mu = snapshot.positions[i]
+                for x in range(ndim):
+                    for y in range(ndim):
+                        medium[i, x, y] = (ndim*mu[x]*mu[y]-kronecker(x,y))/2
+            QIJ.append(medium)
+        QIJ = np.array(QIJ)
+
+        if neighborfile:
+            # coarse-graining over certain volume if neighbor list provided
+            #TODO @Yibang please test this function before test the current function
+            QIJ = spatial_average(
+                input_property=QIJ,
+                neighborfile=neighborfile,
+                Nmax=Nmax,
+            )
+            np.save(outputfile+".QIJ_cg.npy", QIJ)
+        else:
+            np.save(outputfile+".QIJ_raw.npy", QIJ)
+        self.QIJ = QIJ
+
+        logger.info("Compute the scalar nematic order parameter")
+        if eigvals:
+            eigenvalues = np.zeros((QIJ.shape[0], QIJ.shape[1]))
+            for n in range(self.orientations.nsnapshots):
+                for i in range(self.orientations.snapshots[n].nparticle):
+                    eigenvalues[n, i] = np.linalg.eig(QIJ[n,i])[0].max()*2.0
+            np.save(outputfile+".eigval.npy", eigenvalues)
+            return eigenvalues
+        else:
+            Qtrace = np.zeros((QIJ.shape[0], QIJ.shape[1]))
+            for n in range(self.orientations.nsnapshots):
+                for i in range(self.orientations.snapshots[n].nparticle):
+                    Qtrace[n, i] = np.trace(np.matmul(QIJ[n,i], QIJ[n,i]))
+            Qtrace *= ndim / (ndim-1)
+            Qtrace = np.sqrt(Qtrace)
+            np.save(outputfile+".Qtrace.npy", Qtrace)
+            return Qtrace
+        logger.info("Calculate the Nematic order tensor and scalar parameters done")
+
+    def spatial_corr(self):
+        pass
     
-    Return:
-        Q-tensor or eigenvalue scalar nematic order parameter in numpy ndarray format
-        shape as [num_of_snapshots, num_of_particles]
-    """
-    if neighborfile:
-        logger.info(f"Calcualte coarse-grained nematic order for {Snapshots.nsnapshots} configurations")
-    else:
-        logger.info(f"Calcualte original nematic order for {Snapshots.nsnapshots} configurations")
-    # TODO add a function for three dimensioanl systems
-    assert ndim==2, "please set the correction dimensionality"
-
-    QIJ = []
-    for snapshot in Snapshots.snapshots:
-        medium = np.zeros((snapshot.nparticle, ndim, ndim))
-        for i in range(snapshot.nparticle):
-            mu = snapshot.positions[i]
-            for x in range(ndim):
-                for y in range(ndim):
-                    medium[i, x, y] = (ndim*mu[x]*mu[y]-kronecker(x,y))/2
-        QIJ.append(medium)
-    QIJ = np.array(QIJ)
-
-    if neighborfile:
-        # coarse-graining over certain volume if neighbor list provided
-        #TODO @Yibang please test this function before test the current function
-        QIJ = spatial_average(
-            input_property=QIJ,
-            neighborfile=neighborfile,
-            Nmax=Nmax,
-        )
-        np.save(outputfile+".QIJ_cg.npy", QIJ)
-    else:
-        np.save(outputfile+".QIJ_raw.npy", QIJ)
-
-    logger.info("Compute the scalar nematic order parameter")
-    if eigvals:
-        eigenvalues = np.zeros((QIJ.shape[0], QIJ.shape[1]))
-        for n in range(Snapshots.nsnapshots):
-            for i in range(Snapshots.snapshots[n].nparticle):
-                eigenvalues[n, i] = np.linalg.eig(QIJ[n,i])[0].max()*2.0
-        np.save(outputfile+".eigval.npy", eigenvalues)
-        return eigenvalues
-    else:
-        Qtrace = np.zeros((QIJ.shape[0], QIJ.shape[1]))
-        for n in range(Snapshots.nsnapshots):
-            for i in range(Snapshots.snapshots[n].nparticle):
-                Qtrace[n, i] = np.trace(np.matmul(QIJ[n,i], QIJ[n,i]))
-        Qtrace *= ndim / (ndim-1)
-        Qtrace = np.sqrt(Qtrace)
-        np.save(outputfile+".Qtrace.npy", Qtrace)
-        return Qtrace 
-    logger.info("Calculate the Nematic order tensor and scalar parameters done")
+    def time_corr(self):
+        pass
