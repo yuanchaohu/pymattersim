f4387cf466f5b3aaf03078cb16766e0c231bf848
yuanchaohu <ychu0213@gmail.com>
2024-11-19 21:46:40 -0700

    change sphinx theme to furo

--- docs/conf.py
+++ docs/conf.py
@@ -31,5 +31,43 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+# html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
+# html_theme = "sphinx_material"
 html_static_path = ['_static']
+
+# sphinx_rtd_theme
+"""
+html_theme_options = {
+    "collapse_navigation": False,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "style_external_links": True,
+}
+"""
+
+# furo
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#1e88e5",
+        "color-brand-content": "#1e88e5",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#bb86fc",
+        "color-brand-content": "#bb86fc",
+    },
+}
+
+
+# material for sphinx
+"""
+html_theme_options = {
+    "nav_title": "My Project",
+    "color_primary": "blue",
+    "color_accent": "light-blue",
+    "repo_url": "https://github.com/username/repo",
+    "repo_name": "GitHub",
+    "globaltoc_depth": 2,
+}
+"""
