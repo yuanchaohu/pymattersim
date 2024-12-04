05a29fc78f54457079b02ecf95bf6905ff4dc682
yuanchaohu <ychu0213@gmail.com>
2024-12-04 21:49:04 +0800

    add notes for documentation

--- upload.sh
+++ upload.sh
@@ -3,3 +3,8 @@ rm -rf dist/*
 python setup.py sdist bdist_wheel
 twine upload dist/*
 
+# documentation update
+# push changes to master
+# cd docs/
+# make html
+# git subtree push --prefix docs/_build/html origin gh-pages
